df730ede183a285ac276572574038b7fd2ef6b77
Tomas Naiouf <tomasnaiouf@gmail.com>
2021-09-29 11:46:53 -0300

    Corrección de archivos

--- a_global/b_web.ino
+++ a_global/b_web.ino
@@ -1,12 +1,23 @@
 const char *ssid = "Brazo robótico";
-
+const char *pwd = "1234";
+const int mc = 1;
 
 void handleRoot(){
   server.send(200, "text/html", "<h1>Reply from ESP8266</h1>");
 }
 
+void handleNotFound(){
+}
+
+void handleDraw(){
+}
+
+void handleAvailable(){
+}
+
 
 void web_init(){
+  //WiFi.softAP(ssid,pwd,1,false,mc);
   WiFi.softAP(ssid);
   server.on("/", handleRoot);
   server.begin();
