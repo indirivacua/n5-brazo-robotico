1fbbb1c501f81e6ef258335f073901130431ebaf
Oscar Stanchi <indirivacua@hotmail.com>
2021-11-11 23:22:44 -0300

    merge de branch "interfaz_web"

--- app_brdib/brdib/__init__.py
+++ app_brdib/brdib/__init__.py
@@ -1,17 +1,21 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask import Flask                         # Importa la clase Flask, necesaria para correr la aplicación web.
+from flask_sqlalchemy import SQLAlchemy         # Importa la clase SQLAlchemy, necesaria para mapear las tablas de la BDD en objetos en Python.
+from flask_bcrypt import Bcrypt                 # Importa la clase Bcrypt, necesaria para la encriptación de contraseñas de usuarios.
+from flask_login import LoginManager            # Importa la clase LoginManager, necesaria para la administración de sesiones de usuarios.
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///brdib.db'
-app.config['SECRET_KEY'] = '4aba5ad158b07534c64d82d8'
+app = Flask(__name__)                           # Crea una instancia de la clase Flask y le pasa como argumento el nombre de la aplicación,
+                                                # donde __name__ es una variable especial de Python que contiene el nombre del módulo actual.
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///brdib.db'
+app.config['SECRET_KEY'] = \
+    '4aba5ad158b07534c64d82d8'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login_page'
 login_manager.login_message_category = 'info'
-login_manager.login_message = 'Debes iniciar sesión para acceder a esta página.'
+login_manager.login_message = \
+    'Debes iniciar sesión para acceder a esta página.'
 
-from brdib import routes
+from brdib import routes                        # Importa el archivo routes.py del paquete brdib
--- app_brdib/brdib/routes.py
+++ app_brdib/brdib/routes.py
@@ -1,9 +1,12 @@
 from brdib import app
+from flask import session
+from flask_session import Session
 from flask import render_template, redirect, url_for, flash
 from brdib.models import User
-from brdib.form import RegisterForm, LoginForm
+from brdib.form import RegisterForm, LoginForm, UpdateForm
 from brdib import db
 from flask_login import login_user, logout_user, login_required
+from sqlalchemy.orm.attributes import flag_modified
 
 
 # Página Principal (sin sesión activa)
@@ -30,11 +33,12 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         login_user(user_to_create)
+        session['username'] = form.username.data
         flash(f'Éxito! Iniciaste sesión como: {user_to_create.username}', category='success')
         return redirect(url_for('home_page'))
     if form.errors != {}:
-        for error_message in form.errors.values():
-            flash(f'Error al crear usuario: {error_message}')
+        for error_message in form.errors.values():                                                                      # values() retorna una lista con los elementos del diccionario form.errors
+            flash(error_message[0], category="danger")
     return render_template('register.html', form=form)
 
 
@@ -47,10 +51,49 @@ def login_page():
         if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
             login_user(attempted_user)
             flash(f'Éxito! Iniciaste sesión como: {attempted_user.username}', category='success')
+            session['username'] = form.username.data
             return redirect(url_for('home_page'))
         else:
             flash(f'Ups! Usuario o contraseña incorrectos.', category='danger')
     return render_template('login.html', form=form)
+
+
+# Página de Configuración (Modificar Perfil)
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings_page():
+    form = UpdateForm()
+    if form.validate_on_submit():
+        user_to_update = User.query.filter_by(username=session['username']).first()
+        if form.username.data != '':
+            user_to_update.username = form.username.data
+            #flag_modified(user_to_update, 'username')
+        if form.email.data != '':
+            user_to_update.email = form.email.data
+            #flag_modified(user_to_update, 'email')
+        #db.session.merge(user_to_update)
+        #db.session.flush()
+        db.session.commit()
+        session['username'] = form.username.data
+        flash(f'Usuario modificado con éxito!', category='success')
+        return redirect(url_for('home_page'))
+    if form.errors != {}:
+        for error_message in form.errors.values():                                                                      # values() retorna una lista con los elementos del diccionario form.errors
+            flash(error_message[0], category="danger")
+    return render_template('settings.html', form=form)
+
+
+# Página de Baja de Usuarios
+@app.route('/unsubscribe')
+@login_required
+def unsubscribe_page():
+    user_to_delete = User.query.filter_by(username=session['username']).first()
+    logout_user()
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    session.pop('username', None)
+    flash('Baja de usuario realizada con éxito.', category='info')
+    return redirect(url_for('root_page'))
 
 
 # Página de Cierre de Sesión
@@ -58,6 +101,7 @@ def login_page():
 @login_required
 def logout_page():
     logout_user()
+    session.pop('username', None)
     flash('Cerraste sesión correctamente.', category='info')
     return redirect(url_for('root_page'))
 
