1780e1345831dac4fdf83dcca2b657c817a78e7c
tomyyn <tomasnaiouf@gmail.com>
2021-12-05 16:59:50 +0100

    Corregidos errores para prueba

--- a_global/z_main.ino
+++ a_global/z_main.ino
@@ -5,7 +5,8 @@ void setup(){
   delay(1000);
   //Inicializar comunicación wifi y servidor web
   web_init();
-
+  servosInicializar();
+  Serial.begin(9600);
   //Indicar que el brazo se encuentra disponible luego de inicializar todo
   armAvailable = true;
 }
@@ -15,8 +16,12 @@ void loop(){
   //Atender requests del servidor web
   server.handleClient();
   if (!armAvailable){
+    
     dibujoX += orgX;
     dibujoY += orgY;
+    Serial.println(dibujoX);
+    Serial.println(dibujoY);
+    Serial.println(dibujoTamanio);
     if (dibujoTipo == "circle"){
       dibujarCirculo(dibujoX,dibujoY,LP,dibujoTamanio);
     } else if (dibujoTipo == "square"){
